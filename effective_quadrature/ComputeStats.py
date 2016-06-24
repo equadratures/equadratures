cb415e5084d615bbbac839179f7ac82a9c2dec54
Pranay Seshadri <ps583@cam.ac.uk>
2016-06-24 16:23:17 +0100

    Create ComputeStats.py

---
+++ effective_quadrature/ComputeStats.py
@@ -0,0 +1,8 @@
+Set of functions that compute statistics based on the coefficient values and their index set
+We assume coefficients and index sets are correctly computed. 
+
+1. mean
+2. variance
+3. sobol indices - first order and higher
+4. skewness
+5. kurtosis

4574d14ac928bf7b33183042a4506fb1e10efa58
Pranay Seshadri <ps583@cam.ac.uk>
2016-07-03 11:09:45 +0100

    Update Randomized_QR_tests.py

--- tests/Randomized_QR_tests.py
+++ tests/Randomized_QR_tests.py
@@ -1,1 +1,5 @@
+#/usr/bin/python
 
+
+add functionality for randomization -- randomly subsampled stuff. 
+1. Given an array of numbers -- return a Chebyshev + Gaussian random sampling of index sets etc...

4ae941e7f0bb67ff0541f6c9abc69bb0c9df9c46
arifrankel <ari.frankel@gmail.com>
2016-11-30 08:39:54 -0800

    Add files via upload

---
+++ effective_quadratures/Pranay.py
@@ -0,0 +1,32 @@
+import Ylm
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+#Demo 1: 
+#create a triangular type spherical quadrature and plot the points
+def Pranay1():
+    w,theta,phi = Ylm.gen_tri_quad(10)
+    x,y,z = Ylm.sph_to_cart(theta,phi)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x,y,z,s=20*w)
+
+
+#Demo 2:
+#same as demo 1, but with rectangular spherical quadrature
+def Pranay2():
+    w,theta,phi = Ylm.gen_squ_quad(10)
+    x,y,z = Ylm.sph_to_cart(theta,phi)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(x,y,z,s=20*w)
+        
+#Demo 3: check SH integration accuracy
+def Pranay3():
+    w,theta,phi = Ylm.gen_tri_quad(16)
+    x,y,z = Ylm.sph_to_cart(theta,phi)
+    err1, err2 = Ylm.test_quads(x,y,z,w,5)
+    plt.plot(err1)
+    plt.plot(err2)
+    

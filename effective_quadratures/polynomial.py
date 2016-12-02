updates -- still fixing derivatives

effective_quadratures/polynomial.py
@@ -182,7 +182,8 @@ class Polynomial(object):
             return poly, derivatives
         elif dimensions == 1 and stackOfParameters[0].derivative_flag == 1:
             poly , derivatives =  stackOfParameters[0].getOrthoPoly(stackOfPoints)
-            return poly, derivatives
+            C_all[0] = derivatives
+            return poly, C_all
         else:
             for i in range(0, dimensions):
                 p[i] , d[i] = stackOfParameters[i].getOrthoPoly(stackOfPoints[:,i], int(np.max(index_set[:,i] + 1) ) )

tests/WIP_test_gradients.py
@@ -7,6 +7,22 @@ import effective_quadratures.qr as qr
 from effective_quadratures.effectivequads import EffectiveSubsampling
 from effective_quadratures.utils import meshgrid, twoDgrid, evalfunction, evalgradients
 import numpy as np
+" Template for deciding how best to code effectivequads.py"
+
+def getRows(A, row_indices):
+    
+    # Determine the shape of A
+    m , n = A.shape
+
+    # Allocate space for the submatrix
+    A2 = np.zeros((len(row_indices), n))
+
+    # Now loop!
+    for i in range(0, len(A2)):
+        for j in range(0, n):
+            A2[i,j] = A[row_indices[i], j]
+
+    return A2
 
 def fun(x):
     return np.exp(x[0])
@@ -33,58 +49,44 @@ def nogradients_univariate():
     esq = EffectiveSubsampling(parameters)
     
     # Solve the least squares problem
-    A, p, w = esq.getAmatrix()
+    A, p, w = esq.getAmatrix() # Is this always square??
     W = np.mat(np.diag(np.sqrt(w) ) )
     b = W.T  * evalfunction(p, fun)
-    x = qr.solveLSQ( A, b )
+    x = qr.solveLSQ(A,b)
 
 def gradients_univariate():
+    
     # Parameters!
     pt = 6
     x1 = Parameter(param_type="Uniform", lower=-1.0, upper=1.0, points=pt, derivative_flag=1)
-    parameters = [x1]
+    x2 = Parameter(param_type="Uniform", lower=-1.0, upper=1.0, points=pt, derivative_flag=1)
+    parameters = [x1, x2]
+    dims = 1
 
-    # Effective subsampling object!
-    esq = EffectiveSubsampling(parameters)
+    # Basis selection!
+    hyperbolic_cross = IndexSet("Hyperbolic basis", orders=[pt-1,pt-1], q=1.0)
+    esq = EffectiveSubsampling(parameters, hyperbolic_cross)
+    A , p, w = esq.getAmatrix()
+
+    basis_terms_required = hyperbolic_cross.getCardinality() 
+    s = np.int( (basis_terms_required + dims)/(dims + 1.) ) 
     
-    # Solve the least squares problem
-    A, p, w = esq.getAmatrix()
-    C = esq.getCmatrix()
-    W = np.mat(np.diag(np.sqrt(w) ) )
-    b = W.T  * evalfunction(p, fun)
-    d = evalgradients(p, fungrad)
-    x = qr.solveLSQ( A, b )
+    # Now do QR column pivoting on A
+    P = qr.mgs_pivoting(A.T)
+    selected_quadrature_points = P[0:s]
+    Afat =  getRows(np.mat(A), selected_quadrature_points)
 
-#print p, q
-#print C
+    # What is the size of Asub?
+    m, n = Afat.shape
+    esq_pts = getRows(np.mat(quadrature_pts), selected_quadrature_points)
+    esq_wts = quadrature_wts[selected_quadrature_points]
+    W = np.mat(np.diag(np.sqrt(esq_wts)))
+    print m, n
 
-# Now use the formula to determine the least number of subsamples required!
-# Polynomial basis!
-#hyperbolic_cross = IndexSet("Hyperbolic basis", orders=[pt-1,pt-1], q=1.0)
-#esq = EffectiveSubsampling(parameters, hyperbolic_cross)
-#basis_terms_required = hyperbolic_cross.getCardinality() 
-#s = np.int( (basis_terms_required + dims)/(dims + 1.) )
+    f = W.T * evalfunction(esq_pts, fun)
+    #df = evalgradients(esq_pts, fungrad, 'vector')
+    #Csub = esq.getCsubsampled(s)
+    #x = qr.solveLSQ(np.vstack([Asub, Csub]), np.vstack([f, df])  )
+    #print x
 
-#maximum_number_of_evals = hyperbolic_cross.getCardinality()
-
-#print '################## APPLYING QR w/ Hyperbolic cross ####################'
-# Now, lets see what happens if we subsample s number of rows from A using QR!
-#Asub, esq_pts, W, points = esq.getAsubsampled(s)
-#f = W.T * evalfunction(esq_pts, fun)
-#df = evalgradients(esq_pts, fungrad, 'vector')
-#print f, df
-#Csub = esq.getCsubsampled(s)
-
-#x = qr.solveLSQ(np.vstack([Asub, Csub]), np.vstack([f, df])  )
-#print x
-#m, n = Asub.shape
-#print m, n
-#print Csub
-#p, q = Csub.shape
-#print m, n
-#print p, q
-#print Asub
-#print '\n'
-#print Csub
-
-# 1. Stacked least squares!
+gradients_univariate()
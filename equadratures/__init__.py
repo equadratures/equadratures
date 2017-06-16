2b65f6fe4f7fbb43bc3f63d3103778f009223271
Pranay Seshadri <pranay.seshadri@vanderbilt.edu>
2017-06-16 14:18:42 +0100

    checking for python 3 compatibility!

--- equadratures/__init__.py
+++ equadratures/__init__.py
@@ -1,9 +1,9 @@
 'Initialization file'
-from polyint import Polyint
-from parameter import Parameter
-from polylsq import Polylsq
-from polyreg import Polyreg
-from stats import Statistics
-import qr as qr 
-from indexset import IndexSet
-from utils import evalfunction, evalgradients, meshgrid
+from .polyint import Polyint
+from .parameter import Parameter
+from .polylsq import Polylsq
+from .polyreg import Polyreg
+from .stats import Statistics
+import equadratures.qr as qr 
+from .indexset import IndexSet
+from .utils import evalfunction, evalgradients, meshgrid

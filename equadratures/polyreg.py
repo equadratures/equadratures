def551cc06c2d6d043edb880a4e6edb69925d6b7
nicholaswong65656 <nicholaswong65656ster@gmail.com>
2017-11-17 16:32:17 +0000

    Change polyreg

--- equadratures/polyreg.py
+++ equadratures/polyreg.py
@@ -2,11 +2,12 @@
 from .parameter import Parameter
 from .indexset import IndexSet
 import numpy as np
+import scipy
 from math import factorial
 from itertools import combinations
 from .utils import evalfunction, find_repeated_elements, meshgrid
 from .plotting import bestfit, bestfit3D, histogram
-from .qr import solveLSQ
+from .qr import solveLSQ, qr_MGS
 from .stats import Statistics
 
 class Polyreg(object):
@@ -15,170 +16,102 @@ class Polyreg(object):
 
     :param training_x: A numpy 
     :param IndexSet index_set: An instance of the IndexSet class, in case the user wants to overwrite the indices that are obtained using the orders of the univariate parameters in Parameters uq_parameters. The latter corresponds to a tensor grid index set and is the default option if no index_set parameter input is given.
+    :param parameters: List of instances of Parameters class.
+    :param training_y: Column vector (np array) of regression targets corresponding to each row of training_x. Either this or fun should be specified, but not both.
+    :param fun: Function to evaluate training_x on to obtain regression targets automatically. Either this or fun should be specified, but not both.
     
     """
     # Constructor
-    def __init__(self, training_x, training_y, option):
-        self.training_x = training_x
-        self.training_y = training_y
-        self.option = option
-        X = self.training_x
-        Y = self.training_y
-
-        m, n = X.shape
-        ones = np.ones((m, 1))
-        self.dimensions = n
-        #total_terms = nchoosek(n) + order, order)        
-
-        if self.option is 'linear':
-            A = np.mat(np.hstack([X, ones]), dtype='float64')
-            coeffs, not_used = solveLSQ(A, Y)
-            self.coefficients =  np.mat(coeffs, dtype='float64')
-            self.Xmatrix = A
-
-        elif self.option is 'quadratic':
-           dimensions = n
-           variables = range(0, dimensions)
-           combination = list(combinations(variables, 2)) 
-           constants = np.mat(np.ones((m, 1)), dtype='float64')
-
-            # Compute the interaction terms!
-           XC = np.mat( np.ones((m, len(combination))) , dtype='float64')
-           for i in range(0, len(combination) ):
-                for j in range(0, m):
-                    XC[j,i] = X[j, combination[i][0] ] * X[j, combination[i][1] ] ; 
-
-           # Compute the squared terms
-           X2 = np.mat(np.ones((m, dimensions ) ) , dtype = 'float64')
-           for i in range(0, dimensions ):
-                for j in range(0, m):
-                    X2[j,i] = X[j, i] * X[j,i ] ; 
-
-           # Set up the A matrix
-           A = np.mat(np.hstack([constants, X, X2, XC]), dtype = 'float64' )
-           self.Xmatrix = A
-           coeffs, not_used = solveLSQ(A, Y)
-           self.coefficients = np.mat(coeffs, dtype='float64')
-        else:
-            raise(ValueError, 'PolyFit.__init__: invalid fitting option: Choose between linear or quadratic.')
-
-    # Test Polynomial
-    def testPolynomial(self, test_x):
-        """
-        Returns the PDF of the model output. This routine effectively multiplies the coefficients of a polynomial
-        expansion with its corresponding basis polynomials. 
-    
-        :param PolyFit self: An instance of the PolyFit class
-        :param: numpy-matrix test_x: The function that needs to be approximated (or interpolated)
-        :return: polyapprox: The polynomial expansion of a function
-        :rtype: numpy matrix
-
-        """
-        coefficients = self.coefficients
-        p, q = test_x.shape
-        m, dimensions = self.training_x.shape
-        if self.option is 'linear':
-            m = len(coefficients) - 1
-            constant_term = coefficients[m]
-            linear_terms = np.mat(coefficients[0:m], dtype='float64')
-            test_y = np.mat(np.zeros((p, 1)) , dtype='float64' )
-
-            # add a for loop here!
-            for i in range(0, p):
-                test_y[i,0] = (linear_terms.T * test_x[i,:].T) + constant_term
-            return test_y
-
-        elif self.option is 'quadratic':
-            if dimensions == 1:
-                test_y = np.mat(np.zeros((p, 1)) , dtype='float64' )
-                for i in range(0, p):
-                    test_y[i,0] = (self.coefficients[2] * test_x[i,0]**2) + (self.coefficients[1] * test_x[i,:].T) + self.coefficients[0]
-                return test_y
-            else:
-                variables = range(0, dimensions)
-                A = np.mat( np.zeros((dimensions, dimensions)), dtype='float64')
-                c = np.mat( np.zeros((dimensions, 1)), dtype='float64') 
-                combination = list(combinations(variables, 2))
-            
-                # For the interaction terms!
-                for i in range(0, dimensions):
-                    for j in range(0, dimensions):
-                        if j < i :
-                            for k in range(0, len(combination)):
-                                if (combination[k][0] == i and combination[k][1] == j) or (combination[k][1] == i and combination[k][0] == j ) : 
-                                    entry = k
-                            A[i, j] = self.coefficients[dimensions*2 + entry] * 0.5
-                            A[j, i] = A[i, j] # Because A is a symmetric matrix!
-                    A[i,i] = self.coefficients[i + (2*dimensions - 1)] # Diagonal elements of A -- which house the quadratic terms!
-                
-                # For the linear terms!
-                for i in range(0, dimensions):
-                    c[i] = self.coefficients[i+1]        
-                d = self.coefficients[0] # constant term!
-
-                test_y = np.mat(np.zeros((p, 1)) , dtype='float64' )
-                for i in range(0, p):
-                    test_y[i,0] = (test_x[i,:] * A * test_x[i,:].T) + (c.T * test_x[i,:].T) + d
-                return test_y
+    def __init__(self, training_x, parameters, index_set, fun = None, training_y = None):
+        self.x = training_x
+        #Check that x is in the correct shape
+        assert self.x.shape[1] == len(parameters)
         
-    def plot(self, test_x, filename=None):
-        """
-        Returns the PDF of the model output. This routine effectively multiplies the coefficients of a polynomial
-        expansion with its corresponding basis polynomials. 
+        if not((training_y is None) ^ (fun is None)):
+            raise ValueError("Specify only one of fun or training_y.")
+        
+        if not(fun is None):
+            try:
+                self.y = np.apply_along_axis(fun, 1, self.x)
+            except:
+                raise ValueError("Fun must be callable.")
+        else:
+            self.y = training_y                    
+                
+                
+        self.index_set = index_set
+        self.parameters = parameters
+        
+        self.A = getMultivariatePolynomial(parameters, self.x, index_set).T
+        # Make sure y is a column vector
+        self.y = np.reshape(self.y, (len(self.y), 1)) 
     
-        :param Polynomial self: An instance of the Polynomial class
-        :param: callable function: The function that needs to be approximated (or interpolated)
-        :return: polyapprox: The polynomial expansion of a function
-        :rtype: numpy matrix
+    # Solve for coefficients using ordinary least squares
+    def OLS(self):
+        alpha, cond = solveLSQ(self.A, self.y)
+        t_stat = get_t_value(alpha, self.A, self.y)
+        r_sq = get_R_squared(alpha, self.A, self.y)
+        return alpha, cond, t_stat, r_sq
+        
+#TODO: t stat, R square
+#TODO: test hypothesis for given subset specified by given index set
 
-        """
-        dimensions = self.dimensions
-        if dimensions == 1:
-            xx = np.mat(test_x, dtype='float64')
-            test_x = xx.T
-            test_y = self.testPolynomial(test_x)
-            N = len(self.training_y) # number of training points!
+# Get t value for each "variable" (basis term in this case) given regression parameters
+def get_t_value(alpha, A, y):
+    RSS = np.linalg.norm(y - np.dot(A,alpha))**2
+    p = A.shape[1]
+    n = A.shape[0]
+    if n == p:
+        return "exact"
+    RSE = RSS/(n-p)
+    
+    Q, R = qr_MGS(A)
+    inv_ATA = np.linalg.inv(np.dot(R.T, R))
+    se = np.array([np.sqrt(RSE * inv_ATA[j,j]) for j in range(p)])
+    
+    t_stat = alpha / np.reshape(se, (len(se), 1))
+    return t_stat
+    
+def get_R_squared(alpha, A, y):
+    y_bar = scipy.mean(y) * np.ones(len(y))
+    TSS = np.linalg.norm(y - y_bar)**2
+    RSS = np.linalg.norm(np.dot(A,alpha) - y)**2
+    return 1 - RSS/TSS
 
-            if self.option is 'linear':
-                X = self.Xmatrix
-                w = np.linalg.inv(X.T * X) * X.T * self.training_y
-                m, n = test_x.shape
-                ones = np.ones((m, 1))
-                test_X = np.mat(np.hstack([test_x, ones]), dtype='float64')
-                test_mean = test_X * w 
-                ss = 1.0/(1.0 * N) * ( self.training_y.T * self.training_y - self.training_y.T * X * w)
-                test_var = ss * np.diag(test_X * np.linalg.inv(X.T * X) * test_X.T)
-                bestfit(self.training_x, self.training_y, test_x, test_y, test_var.T, r'$X$', r'$Y$', filename)
+# Copied from polyint, removed derivative evaluation
+def getMultivariatePolynomial(stackOfParameters, stackOfPoints, indexsets=None):
+    # "Unpack" parameters from "self"
+    isets = indexsets
+    if indexsets is None:
+        if isets.index_set_type == 'Sparse grid':
+            ic, not_used, index_set = isets.getIndexSet()
+        else:
+            index_set = isets.elements
+    else:
+        index_set = indexsets.elements
 
-            elif self.option is 'quadratic':
-                X = self.Xmatrix
-                w = np.linalg.inv(X.T * X) * X.T * self.training_y
-                m, n = test_x.shape
-                ones = np.ones((m, 1))
-                squares_test_x = np.mat(np.zeros((m , n)), dtype='float64')
-                for i in range(0, m):
-                    squares_test_x[i] = test_x[i]**2
-                test_X = np.mat(np.hstack([ones, test_x, squares_test_x]), dtype='float64')
-                test_mean = test_X * w 
-                ss = 1.0/(1.0 * N) * ( self.training_y.T * self.training_y - self.training_y.T * X * w)
-                test_var = ss * np.diag(test_X * np.linalg.inv(X.T * X) * test_X.T)
-                bestfit(self.training_x, self.training_y, test_x, test_y, test_var.T, r'$X$', r'$Y$', filename)
+    dimensions = len(stackOfParameters)
+    p = {}
 
-        elif dimensions == 2:
-            X1 = test_x[0]
-            X2 = test_x[1]
-            xx1, xx2 = np.meshgrid(X1, X2)
-            u, v = xx1.shape
-            test_x = np.mat( np.hstack( [np.reshape(xx1, (u*v, 1)),  np.reshape(xx2, (u*v, 1)) ]) , dtype='float64')
-            test_y = self.testPolynomial(test_x)
-            yy1 = np.reshape(test_y, (u, v))
-            bestfit3D(self.training_x, self.training_y, [xx1, xx2], yy1, r'$X_1$', r'$X_2$', r'$Y$', filename)
+    # Save time by returning if univariate!
+    if dimensions == 1:
+        poly , _ =  stackOfParameters[0].getOrthoPoly(stackOfPoints)
+        return poly
+    else:
+        for i in range(0, dimensions):
+            if len(stackOfPoints.shape) == 1:
+                stackOfPoints = np.array([stackOfPoints])
+            assert len(stackOfPoints.shape) == 2
+            p[i] , _ = stackOfParameters[i].getOrthoPoly(stackOfPoints[:,i], int(np.max(index_set[:,i] + 1) ) )
 
+    # Now we multiply components according to the index set
+    no_of_points = len(stackOfPoints)
+    polynomial = np.zeros((len(index_set), no_of_points))
 
-# Routine for computing n choose k
-def nchoosek(n, k):
-    numerator = factorial(n)
-    denominator = factorial(k) * factorial(n - k)
-    return (1.0 * numerator) / (1.0 * denominator)
-
-
+    # One loop for polynomials
+    for i in range(0, len(index_set)):
+        temp = np.ones((1, no_of_points))
+        for k in range(0, dimensions):
+            polynomial[i,:] = p[k][int(index_set[i,k])] * temp
+            temp = polynomial[i,:]
+    return polynomial

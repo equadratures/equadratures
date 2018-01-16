7a806dc59920764f40b9c4694b24346f70b05cba
Nicholas Wong <nicholaswong65656ster@gmail.com>
2018-01-16 16:02:25 +0000

    Finish l1 min. Test on a simple example.

--- equadratures/convex.py
+++ equadratures/convex.py
@@ -110,31 +110,51 @@ def CG_solve(A, b, max_iters, tol):
     #Initialization
     x = np.zeros((n,1))
     r = b.copy()
-    p = r.copy()
+
+    d = r.copy()
     iterations = 0
     delta = sum(r**2)
     delta_0 = sum(b**2)
+    bestx = x.copy();
+    bestres = np.sqrt(delta/delta_0); 
     residual = np.sqrt(delta / delta_0)
+#    print delta
     
     while (iterations < max_iters) and (delta > (tol**2) * delta_0):
-        alpha = delta / sum(r * np.dot(A,r))
-        x += alpha * p
-        r -= alpha * np.dot(A,p)
+        
+        q = np.dot(A,d)
+        alpha = delta / sum(d * q)
+        
+        x += alpha * d
+        
+#        if iterations == 1:
+#            print delta
+        r -= alpha * q
+#        print sum(r**2)
         new_delta = sum(r**2)
+#        print new_delta
         beta = new_delta / delta
-        p = r + beta * p
+        d = r + beta * d
         residual = np.sqrt(delta / delta_0)
+        if np.sqrt(delta/delta_0) < bestres:
+            bestx = x.copy();
+            bestres = np.sqrt(delta/delta_0);
+        
         delta = new_delta
         iterations += 1
+#        print "cgiter"
+#        print iterations
+#        print "bestres"
+#        print bestres
     
     
     return x, residual, iterations
     
-def nn_min_l1(A, b, x0 = None, cgtol = None, pdtol = None, pdmaxiter = None):
+def nn_min_l1(A, b, x0 = None, cgtol = None, cgmaxiter = None, pdtol = None, pdmaxiter = None):
     """
     l1 minimization with equality constraint Ax = b.
     (Noiseless basis pursuit)
-    Adapted from l1-magic (Candes and Romberg)
+    Adapted from l1-magic, l1eq_pd.m (Candes and Romberg)
     """
     
     # Free parameters
@@ -143,7 +163,9 @@ def nn_min_l1(A, b, x0 = None, cgtol = None, pdtol = None, pdmaxiter = None):
     mu = 10
     
     if cgtol is None:
-        cgtol = 1e-8
+        cgtol = 1e-5
+    if cgmaxiter is None:
+        cgmaxiter = 200
     if pdtol is None:
         pdtol = 1e-3
     if pdmaxiter is None:
@@ -153,18 +175,20 @@ def nn_min_l1(A, b, x0 = None, cgtol = None, pdtol = None, pdmaxiter = None):
     if not(x0 is None):
         if np.linalg.norm(np.dot(A,x0) - b)/np.linalg.norm(b) > cgtol:
             # Infeasible, use A^t (AA^t)^-1 b (min l2 norm) instead
-            q, cgres, _ = CG_solve(np.dot(A,A.T), b)
+            q, cgres, cgiter = CG_solve(np.dot(A,A.T), b, cgmaxiter, cgtol)
             if cgres > 0.5:
                 raise ValueError('AA^T is too ill conditioned. Cannot find starting point.')
+                xp = x0.copy()
             x0 = np.dot(A.T, q)
     else:
-        q, cgres, _ = CG_solve(np.dot(A,A.T), b)
+        q, cgres, cgiter = CG_solve(np.dot(A,A.T), b, cgmaxiter, cgtol)
         if cgres > 0.5:
             raise ValueError('AA^T is too ill conditioned. Cannot find starting point.')
         x0 = np.dot(A.T, q)
     
-    gradf0 = np.vstack(np.zeros((N,1),dtype = np.float64) np.ones((N,1),dtype = np.float64))
     N = len(x0)
+    gradf0 = np.vstack([np.zeros((N,1),dtype = np.float64), np.ones((N,1),dtype = np.float64)])
+    
     x = x0.copy()
     x = x.reshape((len(x),1))
     u = 0.95*np.abs(x0) + 0.10*np.max(np.abs(x0));
@@ -174,40 +198,113 @@ def nn_min_l1(A, b, x0 = None, cgtol = None, pdtol = None, pdmaxiter = None):
     fu1 = x - u
     fu2 = -x - u
     #initialize lambda to be 1/f
-    lam_u1 = 1.0 / fu1
-    lam_u2 = 1.0 / fu2
+    lam_u1 = -1.0 / fu1
+    lam_u2 = -1.0 / fu2
     #initialize nu (v) to be -A*(lam_u1 - lam_u2)
     v = np.dot(-A, (lam_u1-lam_u2))
     
-    sdg = -(sum(fu1*lamu1) + sum(fu2*lamu2))
-    tau = mu * 2 * N / sdg
+    sdg = -(sum(fu1*lam_u1) + sum(fu2*lam_u2))
+    tau = mu * 2.0 * N / sdg
     
     #Calculate the residuals
     Atv = np.dot(A.T, v)
     r_pri = np.dot(A, x) - b
-    r_cent = np.vstack(-lam_u1 * fu1, -lam_u2 * fu2) - 1.0/tau
-    r_dual = gradf0 + np.vstack(lam_u1 - lam_u2, -lam_u1 - lam_u2) + np.vstack(Atv, np.zeros((N,1)))
+    r_cent = np.vstack([-lam_u1 * fu1, -lam_u2 * fu2]) - 1.0/tau
+    r_dual = gradf0 + np.vstack([lam_u1 - lam_u2, -lam_u1 - lam_u2]) + np.vstack([Atv, np.zeros((N,1))])
     resnorm = np.linalg.norm(np.vstack((r_pri, r_cent, r_dual)))
     
     pditer = 0
     done = (sdg < pdtol) or (pditer >= pdmaxiter)
     
+#    print fu1
+#    print fu2
     while not(done):
         pditer += 1
         
-        # Solve for delta_nu
+        w1 = -1.0/tau*(-1.0/fu1 + 1.0/fu2) - Atv;
+        w2 = -1.0 - 1.0/tau*(1.0/fu1 + 1.0/fu2);
+        w3 = -r_pri;
+        sig1 = -lam_u1/fu1 - lam_u2/fu2;
+        sig2 = lam_u1/fu1 - lam_u2/fu2;
+        sigx = sig1 - sig2**2.0/sig1;
         
-        # Then get delta_x
+        w1p = -w3 + np.dot(A,(w1/sigx - w2*sig2/(sigx*sig1)));
+        inv_sigx = 1.0/sigx
+        H11p = np.dot(np.dot(A,(np.diag(inv_sigx.reshape(len(inv_sigx))))),A.T);
+        dv, cgres, cgiter = CG_solve(H11p, w1p, cgmaxiter, cgtol);
+        if cgres > .5:
+#            print cgres
+            raise ValueError('Matrix ill-conditioned.  Returning previous iterate.');
+            xp = x.copy()
+            return
+        dx = (w1 - w2*sig2/sig1 - np.dot(A.T,dv))/sigx
+        Adx = np.dot(A,dx)
+        Atdv = np.dot(A.T,dv)
+
         
-        # Then get delta_u
+        du = (w2 - sig2*dx)/sig1;
+  
+        dlamu1 = (lam_u1/fu1)*(-dx+du) - lam_u1 - (1.0/tau)*1.0/fu1;
+        dlamu2 = (lam_u2/fu2)*(dx+du) - lam_u2 - 1.0/tau*1.0/fu2;
         
-        # Then get delta_lambda for both u1 and u2
+        # make sure that the step is feasible: keeps lam_u1,lam_u2 > 0, fu1,fu2 < 0
+#        indp = find(dlamu1 < 0);  indn = find(dlamu2 < 0);
+        s = np.min(np.hstack([1.0, -lam_u1[dlamu1 < 0]/dlamu1[dlamu1 < 0], -lam_u2[dlamu2 < 0]/dlamu2[dlamu2 < 0]]));
+#        indp = find((dx-du) > 0);  indn = find((-dx-du) > 0);
+        s = (0.99)*np.min(np.hstack([s, -fu1[(dx-du) > 0] / (dx[(dx-du) > 0] - du[(dx-du) > 0]) , -fu2[(-dx-du) > 0] / (-dx[(-dx-du) > 0] - du[(-dx-du) > 0]) ] ))
+      
+        # backtracking line search
+        suffdec = 0;
+        backiter = 0;
+        while not(suffdec):
+            xp = x + s*dx;
+            up = u + s*du; 
+            vp = v + s*dv;
+            Atvp = Atv + s*Atdv; 
+            lamu1p = lam_u1 + s*dlamu1;
+            lamu2p = lam_u2 + s*dlamu2;
+            fu1p = xp - up;
+            fu2p = -xp - up;  
+            rdp = gradf0 + np.vstack([lamu1p-lamu2p, -lamu1p-lamu2p]) + np.vstack([Atvp, np.zeros((N,1))]);
+            rcp = np.vstack([-lamu1p*fu1p, -lamu2p*fu2p]) - (1.0/tau);
+            rpp = r_pri + s*Adx;
+            suffdec = (np.linalg.norm(np.vstack([rdp, rcp, rpp])) <= (1.0-alpha*s)*resnorm);
+            s = beta*s;
+            backiter = backiter + 1;
+            if backiter > 32:
+              print 'Stuck backtracking, returning last iterate.'
+              xp = x;
+              return
+
+      
+      
+        # next iteration
+        x = xp.copy();
+        u = up.copy();
+        v = vp.copy();
+        Atv = Atvp.copy(); 
+        lam_u1 = lamu1p.copy();
+        lam_u2 = lamu2p.copy();
+        fu1 = fu1p.copy();
+        fu2 = fu2p.copy();
+      
+        # surrogate duality gap
+
+        sdg = -(np.sum(fu1*lam_u1) + np.sum(fu2*lam_u2));
+        tau = mu*2.0*N/sdg;
+        r_pri = rpp.copy();
+        r_cent = np.vstack([-lam_u1*fu1, -lam_u2*fu2]) - (1.0/tau);
+        r_dual = gradf0 + np.vstack([lam_u1-lam_u2, -lam_u1-lam_u2]) + np.vstack([Atv, np.zeros((N,1))]);
+        resnorm = np.linalg.norm(np.vstack([r_dual, r_cent, r_pri]));
+      
+        done = (sdg < pdtol) | (pditer >= pdmaxiter);
+      
+        print "Iteration = " + str(pditer) + ", tau = " + str(tau) + ", Primal = " + str(sum(u)) + ", PDGap = " + str(sdg) \
+            + ", Dual res = " + str(np.linalg.norm(r_dual)) + ", Primal res = " + str(np.linalg.norm(r_pri))
         
-        # Then get step size:
-        # 1. Must be in interior
-        # 2. Must decrease resnorm sufficiently
-        
-        # Prepare for next iteration
+        print "CG Res = " + str(cgres) + "CG Iter" + str(cgiter)
+
+    return xp  
 
 def binary2indices(zhat):
     """

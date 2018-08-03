8527b7c9474620907417ffb8044daffb1f9334c9
irevirdis <39519913+irevirdis@users.noreply.github.com>
2018-08-03 21:24:53 +0200

    Update cauchy.py

--- equadratures/distributions/cauchy.py
+++ equadratures/distributions/cauchy.py
@@ -22,7 +22,7 @@ class Cauchy(Distribution):
         self.variance = np.nan
         self.skewness = np.nan
         self.kurtosis = np.nan
-        self.x_range_for_pdf = np.linspace(-15*self.scale. 15*self.scale, RECURRENCE_PDF_SAMPLES)
+        self.x_range_for_pdf = np.linspace(-15*self.scale, 15*self.scale, RECURRENCE_PDF_SAMPLES)
     
     def getDescription(self):
         """

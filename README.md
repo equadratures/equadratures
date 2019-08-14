f5f850a6ff0479db5281e4deb44506223f511825
nicholaswong65656 <nicholaswong65656ster@gmail.com>
2019-08-14 15:30:27 +0100

    merge

---
+++ README.md
@@ -0,0 +1,10 @@
+# Effective Quadratures
+**Version 8.0**
+
+Tools for machine learning with polynomials.
+
+# Installation and Documentation
+Please visit www.effective-quadratures.org
+
+# Community guidelines
+If you have contributions, questions, or feedback use either the Github repository, or contact: contact <at> effective-quadratures.org
---
+++ equadratures/__init__.py
@@ -0,0 +1,12 @@
+from equadratures.parameter import Parameter
+from equadratures.poly import Poly
+from equadratures.stats import Statistics
+from equadratures.basis import Basis
+from equadratures.correlations import Correlations
+from equadratures.optimisation import Optimisation
+from equadratures.subspaces import Subspaces
+from equadratures.poly import evaluate_model, evaluate_model_gradients, vector_to_2D_grid
+import numpy as np
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+VERSION_NUMBER = 8.0

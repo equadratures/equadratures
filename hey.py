82da82cf290d7ae9673ba1bcf8ec93051544bec5
hz324 <joezhouhe@outlook.com>
2019-08-15 17:29:23 +0100

    clean up plotting to hey.py as a simple verification

--- hey.py
+++ hey.py
@@ -2,12 +2,21 @@ from equadratures.induced_sampling import InducedSampling
 from equadratures.parameter import Parameter
 from equadratures.basis import Basis
 
-dimension = 3 
+import numpy as np
+from matplotlib.pyplot import imshow
+import matplotlib.pyplot as plt
+
+dimension = 3
 sampling_ratio = 10
 parameters = [Parameter(3, "Uniform", upper=1, lower=-1)]*dimension
 basis = Basis("total order", [3]*dimension)
 
 induced_sampling = InducedSampling(parameters, basis, sampling_ratio, "qr")
 
-quadrature_points = induced_sampling.samples()
+quadrature_points, weights, polynomial = induced_sampling.samples()
 
+ATA = np.matmul(np.matmul(polynomial, np.diag(weights)), polynomial.transpose())
+print(ATA.shape)
+imshow(ATA)
+plt.colorbar()
+plt.show()

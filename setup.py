60532dfb787b84ec0a9add6f496d8f1b81447171
matrix <ps583@cam.ac.uk>
2019-08-20 20:23:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='equadratures',
-      version='8.0.0',
+      version='8.0.1',
       description='Machine learning with polynomials',
       long_description=readme(),
       classifiers=[

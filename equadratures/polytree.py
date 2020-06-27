9412dbadaaa73b2741fa6b7b51abb0798da3e220
Barney Hill <you@example.com>
2020-06-27 12:15:05 +0100

    Updated docs and logging

--- equadratures/polytree.py
+++ equadratures/polytree.py
@@ -6,7 +6,40 @@ from equadratures.basis import Basis
 from urllib.parse import quote
 
 class PolyTree(object):
+	"""
+    Definition of a polynomial tree object.
 
+    :param int max_depth:
+    	The maximum depth to which the tree will search to.
+    :param int min_samples_leaf:
+    	The minimum number of samples per leaf node.
+    :param int order:
+    	The order of the generated orthogonal polynomials.
+    :param str basis:
+    	The type of index set used for the basis. Options include: ``univariate``, ``total-order``, ``tensor-grid``, ``sparse-grid``, ``hyperbolic-basis`` [1] and ``euclidean-degree`` [2]; all basis are isotropic. 
+    :param str search:
+    	The method of search to be used. Options include ``uniform`` and ``exhaustive``
+    :param int samples:
+    	The interval between splits if ``uniform`` search is chosen
+    :param bool logging:
+    	Actions saved to log
+
+    **Sample constructor initialisations**::
+
+        import numpy as np
+        from equadratures import *
+
+        tree = polytree.PolyTree()
+
+        X = np.loadtxt('inputs.txt')
+        y = np.loadtxt('outputs.txt')
+        
+        tree.fit(X,y)
+
+    **References**
+        1. Blatman, G., Sudret, B., (2011) Adaptive Sparse Polynomial Chaos Expansion Based on Least Angle Regression. Journal of Computational Physics, 230(6), 2345-2367.
+        2. Trefethen, L., (2017) Multivariate Polynomial Approximation in the Hypercube. Proceedings of the American Mathematical Society, 145(11), 4837-4844. `Pre-print <https://arxiv.org/pdf/1608.02216v1.pdf>`
+    """
 	def __init__(self, max_depth=5, min_samples_leaf=10, order=3, basis='tensor-grid', search='exhaustive', samples=10, logging=False):
 		self.max_depth = max_depth
 		self.min_samples_leaf = min_samples_leaf
@@ -19,6 +52,13 @@ class PolyTree(object):
 		self.log = []
 
 	def get_polys(self):
+		"""
+		Returns the list of polynomials used in the tree
+
+		:return:
+			**polys**: A list of Poly objects
+		"""
+
 		def _search_tree(node, polys):
 			if node["children"]["left"] == None and node["children"]["right"] == None:
 				polys.append(node["poly"])
@@ -34,6 +74,14 @@ class PolyTree(object):
 		return _search_tree(self.tree, [])
 
 	def fit(self, X, y):
+		"""
+		Fits the tree to the provided data
+
+		:param numpy.ndarray X:
+			Training input data
+		:param numpy.ndarray y:
+			Training output data
+		"""
 
 		def _build_tree():
 
@@ -87,7 +135,7 @@ class PolyTree(object):
 							
 							# Update best parameters if loss is lower
 							if loss_split < loss_best:
-								if self.logging: self.log.append({'event': 'best_split', 'data': {'j_feature':j_feature, 'threshold':threshold, 'loss': loss_split}})
+								if self.logging: self.log.append({'event': 'best_split', 'data': {'j_feature':j_feature, 'threshold':threshold, 'loss': loss_split, 'poly_left': poly_left, 'poly_right': poly_right}})
 								did_split = True
 								loss_best = loss_split
 								polys_best = [poly_left, poly_right]
@@ -95,7 +143,7 @@ class PolyTree(object):
 								j_feature_best = j_feature
 								threshold_best = threshold
 	
-							elif self.logging: self.log.append({'event': 'try_split', 'data': {'j_feature':j_feature, 'threshold':threshold, 'loss': loss_split}})
+							elif self.logging: self.log.append({'event': 'try_split', 'data': {'j_feature':j_feature, 'threshold':threshold, 'loss': loss_split, 'poly_left': poly_left, 'poly_right': poly_right}})
 				# Return the best result
 				result = {"did_split": did_split,
 						  "loss": loss_best,
@@ -126,7 +174,7 @@ class PolyTree(object):
 				poly = Poly(myParameters, myBasis, method='least-squares', sampling_args={'sample-points':X, 'sample-outputs':y})
 				poly.set_model()
 				
-				mse = float(sum([(y[i]-poly.get_polyfit(X)[i]) ** 2 for i in range(N)])) / N
+				mse = np.linalg.norm(y - poly.get_polyfit(X).reshape(-1)) ** 2 / N
 				return mse, poly
 
 			def _split_data(j_feature, threshold, X, y):
@@ -188,6 +236,14 @@ class PolyTree(object):
 		self.tree = _build_tree()
 	
 	def predict(self, X):
+		"""
+		Evaluates the the polynomial tree approximation of the data.
+
+		:param numpy.ndarray X:
+			An ndarray with shape (number_of_observations, dimensions) at which the tree fit must be evaluated at.
+		:return: **y**:
+			A numpy.ndarray of shape (1, number_of_observations) corresponding to the polynomial approximation of the tree.
+		"""
 		assert self.tree is not None
 		def _predict(node, x):
 			no_children = node["children"]["left"] is None and \
@@ -203,12 +259,13 @@ class PolyTree(object):
 		y_pred = np.array([_predict(self.tree, np.array(x)) for x in X])
 		return y_pred
 
-	def loss(self, y, y_pred):
-		mse = sum([(y[i]-y_pred[i]) ** 2 for i in range(len(y))]) / len(y)
-		return mse
-
 	def get_graphviz(self, feature_names):
-			
+		"""
+		Returns a url to the rendered graphviz representation of the tree.
+
+		:param list feature_names:
+			A list of the names of the features used in the training data
+		"""
 		from graphviz import Digraph
 		g = Digraph('g', node_attr={'shape': 'record', 'height': '.1'})
 
@@ -225,10 +282,10 @@ class PolyTree(object):
 			else:
 				threshold_str = "{} <= {:.1f}\\n".format(feature_names[node['j_feature']], node["threshold"])
 			
-			indices = []
-			for i in range(len(feature_names)):
-				indices.append("{} : {}\\n".format(feature_names[i], node["poly"].get_sobol_indices(1)[i,]))
-			label_str = "{} n_samples = {}\\n loss = {:.6f}\\n sobol indices: {}".format(threshold_str, node["n_samples"], node["loss"], [i for i in indices])
+			#indices = []
+			#for i in range(len(feature_names)):
+			#	indices.append("{} : {}\\n".format(feature_names[i], node["poly"].get_sobol_indices(1)[i,]))
+			label_str = "{} n_samples = {}\\n loss = {:.6f}".format(threshold_str, node["n_samples"], node["loss"])
 
 			# Create node
 			nodeshape = "rectangle"

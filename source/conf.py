6877510dcbac95cc0aebf552061ce407f3225af6
Pranay Seshadri <p.seshadri@imperial.ac.uk>
2021-01-21 08:46:05 +0000

    WIP

--- source/conf.py
+++ source/conf.py
@@ -1,10 +1,10 @@
 import os
 import sys
 project = u' '
-copyright = u'2016-2020 by Effective Quadratures'
+copyright = u'2016-2021 by Effective Quadratures'
 author = u'Effective Quadratures'
 # -- Bryn: Main Vuepress website location ------------------------------------
-land_page = "https://www.effective-quadratures.org/"
+land_page = "equadratures.org"
 outdir = sys.argv[-1]   #Bryn: Assuming last system argument is output directory
 version = u'9.0'
 release = u'v9.0.1'
@@ -18,10 +18,12 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.slide',
+    'sphinxcontrib.youtube',
 ]
 templates_path = ['_docstemplates']
 source_suffix = '.txt'
-master_doc = '_documentation/index'
+master_doc = 'index'
 html_title = 'equadratures'
 html_theme = 'pydata_sphinx_theme'
 html_logo = 'logo_new.png'
@@ -32,50 +34,36 @@ language = None
 exclude_patterns = [u'_docsbuild', 'Thumbs.db', '.DS_Store']
 html_static_path = ['_static']
 htmlhelp_basename = 'EffectiveQuadraturesdoc'
-
 latex_elements = {
     'papersize': 'a4paper',
     'pointsize': '12pt',
 }
-
-latex_documents = [
-    (master_doc, 'EffectiveQuadratures.tex', u'Effective Quadratures Documentation',
-     u'Pranay Seshadri, Nicholas Wong, Henry Yuchi, Irene Virdis', 'manual'),
-]
-
 man_pages = [
     (master_doc, 'effectivequadratures', u'Effective Quadratures Documentation',
      [author], 1)
 ]
-
 texinfo_documents = [
-    (master_doc, 'EffectiveQuadratures', u' ',
-     author, 'EffectiveQuadratures', 'One line description of project.',
+    (master_doc, 'equadratures', u' ',
+     author, 'equadratures', 'equadratures is an open-source python code developed by Effective Quadratures. It is tailored for tackling problems in uncertainty quantification, surrogate-based optimisation, numerical integration, and data-driven dimension reduction. ',
      'Miscellaneous'),
 ]
-
 intersphinx_mapping = {'https://docs.python.org/': None}
-
 html_theme_options = {
+    "external_links": [
+      {"url": "https://discourse.equadratures.org/", "name": "Discourse"}
+     ],
     'nosidebar': True,
-    'sidebar_span': 0,
-    "github_url": "https://github.com/pandas-dev/pydata-sphinx-theme",
-    "twitter_url": "https://twitter.com/EQuadratures",
+    "github_url": "https://github.com/Effective-Quadratures/equadratures",
+    "twitter_url": "https://twitter.com/equadratures",
     'globaltoc_includehidden': "true",
     "search_bar_position": "navbar",
      "show_prev_next": False,
      "search_bar_text": "Search equadratures..."
 }
-
 html_sidebars = {
+  " /_documentation/tutorials": [],
   "tutorials": [],
   "index": [],
+  " /_documentation/modules":[],
   "modules":[]
-}
-
-html_context = {
-    "github_user": "pandas-dev",
-    "github_repo": "pydata-sphinx-theme",
-    "github_version": "master",
-    "doc_path": "docs",
 }

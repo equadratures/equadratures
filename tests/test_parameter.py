57beedaf85ec75593af43fcb13e091d6de923990
Simardeep27 <simardeep2720@gmail.com>
2021-08-26 13:57:17 +0530

    Added test case for data parameter

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -13,5 +13,20 @@ class TestParameter(TestCase):
         myparameter2 = Parameter(lower=250., upper=300., variable='horsepower')
         np.testing.assert_equal(myparameter2.variable, 'horsepower')
 
+    def test_param_data(self):
+        arr=np.linspace(0,100,200)
+        myparameter=Parameter(data=arr)
+        np.testing.assert_equal(myparameter.variable, 'parameter')
+        np.testing.assert_equal(myparameter.order, 1)
+        myparameter2=Parameter(lower=min(arr),upper=max(arr))
+        mybasis=Basis('total-order')
+        myPoly=Poly(myparameter,mybasis,method='least-squares')
+        myPoly2=Poly(myparameter2,mybasis,method='least-squares')
+
+        np.testing.assert_allclose(myPoly.get_points_and_weights()[0],myPoly2.get_points_and_weights()[0],
+                                   err_msg = "Difference greated than imposed tolerance")
+        np.testing.assert_allclose(myPoly.get_points_and_weights()[1], myPoly2.get_points_and_weights()[1],
+                                   err_msg="Difference greated than imposed tolerance")
+
 if __name__== '__main__':
     unittest.main()

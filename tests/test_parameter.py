c233a5699da33bbafec40d4bbd5f405972773677
Simardeep27 <simardeep2720@gmail.com>
2021-08-27 00:31:53 +0530

    Added tests for exponentional, gauss, lognormal

--- tests/test_parameter.py
+++ tests/test_parameter.py
@@ -16,7 +16,7 @@ class TestParameter(TestCase):
         myparameter2 = Parameter(lower=250., upper=300., variable='horsepower')
         np.testing.assert_equal(myparameter2.variable, 'horsepower')
 
-    def test_param_data(self):
+    def test_param_data_uniform(self):
         lower=5
         upper=10
         ScipyParam=stats.uniform(loc=lower,scale=upper-lower)
@@ -26,6 +26,37 @@ class TestParameter(TestCase):
 
         np.testing.assert_almost_equal(myparam.mean,myparam2.mean,decimal=3,err_msg ="Difference_{}")
         np.testing.assert_almost_equal(myparam.variance, myparam2.variance, decimal=3, err_msg="Difference_{}")
+
+    def test_param_data_expon(self):
+        shape_parameter_A=5
+        ScipyParam=stats.expon(scale=1/shape_parameter_A,loc=0)
+        myparam=Parameter(distribution='exponential',shape_parameter_A=shape_parameter_A)
+        arr=ScipyParam.rvs(size=1000,random_state=2021)
+        myparam2=Parameter(distribution='exponential',data=arr)
+
+        np.testing.assert_almost_equal(myparam.mean,myparam2.mean,decimal=3,err_msg ="Difference_{}")
+        np.testing.assert_almost_equal(myparam.variance, myparam2.variance, decimal=3, err_msg="Difference_{}")
+
+    def test_param_data_lognorm(self):
+        shape_parameter=0.15
+        ScipyParam=stats.lognorm(s=shape_parameter,loc=0)
+        myparam=Parameter(distribution='lognormal',shape_parameter_A=shape_parameter)
+        arr=ScipyParam.rvs(size=1000,random_state=2021)
+        myparam2=Parameter(distribution='lognormal',data=arr)
+
+        np.testing.assert_almost_equal(myparam.mean,myparam2.mean,decimal=2,err_msg ="Difference_{}")
+        np.testing.assert_almost_equal(myparam.variance, myparam2.variance, decimal=2, err_msg="Difference_{}")
+
+    def test_param_data_gauss(self):
+        shape_parameter_A=5
+        shape_parameter_B=10
+        ScipyParam=stats.norm(loc=shape_parameter_A,scale=np.sqrt(shape_parameter_B))
+        myparam=Parameter(distribution='gaussian',shape_parameter_A=shape_parameter_A,shape_parameter_B=shape_parameter_B)
+        arr=ScipyParam.rvs(size=1000,random_state=2021)
+        myparam2=Parameter(distribution='gaussian',data=arr)
+
+        np.testing.assert_almost_equal(myparam.mean,myparam2.mean,decimal=1,err_msg ="Difference_{}")
+        np.testing.assert_almost_equal(myparam.variance, myparam2.variance, decimal=1, err_msg="Difference_{}")
 
 if __name__== '__main__':
     unittest.main()
